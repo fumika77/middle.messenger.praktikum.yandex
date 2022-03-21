473a3513301d3a6311757e80b693a67efd0ef67e
Maria <cherdantseva.m@yandex.ru>
2022-03-21 20:43:32 +0300

    Обновлен index.js

--- index.js
+++ index.js
@@ -3,5 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static('./dist'))
-
+app.get('/', function(req, res) {
+    res.send(__dirname + 'index.html');
+});
 app.listen(port, () => console.log(`App listening to port ${port}`));
